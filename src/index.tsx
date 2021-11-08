b6360cdbcc3cd2899253677a5f6df7e883211428
Ruslanchik <Ruselkadach@mail.ru>
2021-11-08 21:52:46 +0300

    -deleted hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,13 @@ import './index.css';
 import App from './App';
 import store from "./store/store";
 import {Provider} from "react-redux";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
-        </Provider>
-    </React.StrictMode>,
+        </Provider>,
     document.getElementById('root')
 );
