3d4be45e772eebc45e5c075388932203e9784846
Ruslanchik <Ruselkadach@mail.ru>
2021-11-15 22:53:00 +0300

    -change apple touch icon

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from "./store/store";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+        <Provider store={store}>
+            <HashRouter>
+                <App/>
+            </HashRouter>
+        </Provider>,
+    document.getElementById('root')
 );
